6a6f711b8cb1c5364e43cb14b9c00422024d5ec4
user2 <user2@DESKTOP-MRT7GU2>
2022-11-17 08:54:23 +0900

    new one

---
+++ src/sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum {
+
+	public static void main(String[] args) {
+		System.out.println("두 숫자의 합을 구해드립니다 숫자 두개를 입력하세요");
+		Scanner input = new Scanner(System.in);
+		int first = input.nextInt();
+		int second = input.nextInt();
+		System.out.println("두수의 합은 :"+(first+second)+"입니다");
+		input.close();
+	}
+
+}
