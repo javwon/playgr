a340105a6d702313ccf20bcd7937c4d53384afe9
user2 <user2@DESKTOP-MRT7GU2>
2022-11-17 18:21:12 +0900

    delicate calendar (teacher)

--- src/Prompt.java
+++ src/Prompt.java
@@ -3,38 +3,55 @@ import java.util.Scanner;
 public class Prompt {
 	
 	private final static String PROMPT="cal >";
-	
+	public int parseDay(String day)
+	{	
+		
+		if(day.equals("su"))
+			return 0;
+		else if (day.equals("mo"))return 1;
+		else if (day.equals("tu"))return 2;
+		else if (day.equals("we"))return 3;
+		else if (day.equals("th"))return 4;
+		else if (day.equals("fr"))return 5;
+		else if (day.equals("sa"))return 6;
+		else return 0;
+	}
 	public void runPrompt(){
 		Scanner scanner =new Scanner(System.in);
 		play Max_days = new play();
-		Scanner ds = new Scanner(System.in);
+		int year=0;
+		int month =-1;
+		int day =0;
 		while(true)
 		{	
-		System.out.println("\n년도를 입력하세요");
+		System.out.println("\n년도를 입력하세요(-1 종료)");
 		System.out.print(PROMPT);
-		int year = scanner.nextInt();
+		year = scanner.nextInt();
+		if(year == -1)break;
 		System.out.println("\n달을 입력하세요(-1이면 종료)");
 		System.out.print(PROMPT);
-		int month = scanner.nextInt();
+		month = scanner.nextInt();
 		
 		System.out.println("\n 시작하는 날짜를 입력하세요");
 		System.out.print(PROMPT);
-		String day = ds.nextLine();
+		String days = scanner.next(); //next line으로 하면 에러 났었는데 next로 해결가능할까?
+		day=parseDay(days);
 		
 		if(month>=1 && month <=12)
 			
 		{
 			Max_days.printSample(year,month,day);
 		}
-		else if (month == -1)
-			break;
-		else
+		
+		else {
+			System.out.println("잘못 된 입력입니다.");
 			continue;
+		}
 		}
 		System.out.println("bye~");
 //		Max_days.printSample();
 		scanner.close();
-		ds.close();
+		
 	}
 	
 	public static void main(String[] args) {
--- src/play.java
+++ src/play.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 public class play {
 	private static final int[] MONTH_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
 	private static final int[] MONTH_DAYS2 = {31,29,31,30,31,30,31,31,30,30,31,31};
@@ -16,34 +16,35 @@ public class play {
 		else
 			return MONTH_DAYS[month-1];
 	}
-	public int spaceday(String day)
-	{
-		String[] days = {"su","mo","tu","we","th","fr","sa"};
-		int i=0;
-		for(;i<days.length;i++)
-		{
-			if(days[i].equalsIgnoreCase(day))
-				break;
-		}
-		return i;
-	}
-	public void printSample(int year,int month,String day)
+
+
+	public void printSample(int year,int month,int day)
 	{	
 		
 		System.out.printf("    <<%4d%3d>>%n",year,month);
 		System.out.println("   일  월   화   수   목   금   토");
 		System.out.println(" --------------------");
-	
+		
+		for(int i=0;i<day;i++)
+		{
+			System.out.printf("   ");
+		}
+		
 		int maxdayofmonth = maxday(year,month);
-		int j =spaceday(day);
-		for(int i =1,k=1; i<=maxdayofmonth+j;i++)
-		{	if(i<j+1)
-			{System.out.printf("%3s"," ");
-			}
-			else
-			{System.out.printf("%3d", k);
-			 k++;}
-			if(i % 7 ==0)
+		int count =7-day; //시작라인 숫자 맞춰주려고함 출력갯수
+//		int delim = (count < 7)? count:0;
+		//print first line
+		for(int i=1;i<=count;i++)
+		{
+			System.out.printf("%3d",i);
+		}
+		System.out.println();
+		//print second line
+		for(int i =count+1; i<=maxdayofmonth;i++)
+		{	
+			System.out.printf("%3d", i);
+			 
+			if((i-count) % 7 ==0) //다른방법으론 나머지가 count만큼 되도록 설정
 				System.out.println();
 		}
 
