1c500d514cb1ba138a9d408ba590343792f3fa66
user2 <user2@DESKTOP-MRT7GU2>
2022-11-18 11:09:24 +0900

    Gregorian Algorithm

--- src/Prompt.java
+++ src/Prompt.java
@@ -3,25 +3,13 @@ import java.util.Scanner;
 public class Prompt {
 	
 	private final static String PROMPT="cal >";
-	public int parseDay(String day)
-	{	
-		
-		if(day.equals("su"))
-			return 0;
-		else if (day.equals("mo"))return 1;
-		else if (day.equals("tu"))return 2;
-		else if (day.equals("we"))return 3;
-		else if (day.equals("th"))return 4;
-		else if (day.equals("fr"))return 5;
-		else if (day.equals("sa"))return 6;
-		else return 0;
-	}
+
 	public void runPrompt(){
 		Scanner scanner =new Scanner(System.in);
 		play Max_days = new play();
 		int year=0;
 		int month =-1;
-		int day =0;
+		
 		while(true)
 		{	
 		System.out.println("\n년도를 입력하세요(-1 종료)");
@@ -32,15 +20,11 @@ public class Prompt {
 		System.out.print(PROMPT);
 		month = scanner.nextInt();
 		
-		System.out.println("\n 시작하는 날짜를 입력하세요");
-		System.out.print(PROMPT);
-		String days = scanner.next(); //next line으로 하면 에러 났었는데 next로 해결가능할까?
-		day=parseDay(days);
-		
+
 		if(month>=1 && month <=12)
 			
 		{
-			Max_days.printSample(year,month,day);
+			Max_days.printSample(year,month);
 		}
 		
 		else {
--- src/play.java
+++ src/play.java
@@ -16,35 +16,62 @@ public class play {
 		else
 			return MONTH_DAYS[month-1];
 	}
-
-
-	public void printSample(int year,int month,int day)
+	public int findactualday(int total)
+	{
+		if(total == 0)
+			return 6;
+		else return total-1;
+		
+	}
+	
+	public int find_Lep_year (int year)
+	{
+		return ((int)((year-1)/4)-(int)((year-1)/100)+(int)((year-1)/400));
+	}
+	
+	public int findofday(int year, int month)
+	{	int[] arr = isLeapYear(year) ? MONTH_DAYS2:MONTH_DAYS;
+		//평년 합 윤년 합 구하기 기준년도는 1583년
+		int year_discount = year-1583;
+		int Lep_year = find_Lep_year(year) - find_Lep_year(1583);
+		int nomar_year = year_discount - Lep_year;
+		//해당년도 1월1일이 몇 요일인지 나온다. 토요일 기준
+		int total_count = (nomar_year + (Lep_year*2)) % 7;
+		//해당 년도가 정확히 몇요일인지 나오는 것 
+		int counter =findactualday(total_count);
+		
+		int year_month_sum=0;
+		
+		for(int i=0;i<(month-1);i++)
+		{
+			year_month_sum += arr[i];
+		}
+		
+		return (counter+(year_month_sum))%7;
+	}
+	
+	public void printSample(int year,int month)
 	{	
 		
 		System.out.printf("    <<%4d%3d>>%n",year,month);
 		System.out.println("   일  월   화   수   목   금   토");
 		System.out.println(" --------------------");
 		
-		for(int i=0;i<day;i++)
+	
+		int maxdayofmonth = maxday(year,month);
+		int counter = findofday(year,month);
+		
+		for(int i=0;i<counter;i++)
 		{
-			System.out.printf("   ");
+			System.out.printf("%3s"," ");
 		}
 		
-		int maxdayofmonth = maxday(year,month);
-		int count =7-day; //시작라인 숫자 맞춰주려고함 출력갯수
-//		int delim = (count < 7)? count:0;
-		//print first line
-		for(int i=1;i<=count;i++)
-		{
-			System.out.printf("%3d",i);
-		}
-		System.out.println();
 		//print second line
-		for(int i =count+1; i<=maxdayofmonth;i++)
+		for(int i =1; i<=maxdayofmonth;i++)
 		{	
 			System.out.printf("%3d", i);
 			 
-			if((i-count) % 7 ==0) //다른방법으론 나머지가 count만큼 되도록 설정
+			if((i+counter) % 7 ==0) //다른방법으론 나머지가 count만큼 되도록 설정
 				System.out.println();
 		}
 
