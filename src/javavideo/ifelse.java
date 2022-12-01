02e5f67c9ec972e82aec31c6f4262a18ccbc1a67
mimi5963 <mimi5963@naver.com>
2022-12-01 16:35:13 +0900

    hmmm

---
+++ src/javavideo/ifelse.java
@@ -0,0 +1,16 @@
+package javavideo;
+
+public class ifelse {
+
+	
+	static void printChoice() {
+		System.out.println("choices Available are");
+		for(int i=0; i<4;i++)
+			{String[] str= {"1-Add","2-Subteract","3-divide","4-Muliply"};
+				System.out.println(i);
+			
+			}
+		
+		
+	}
+}
---
+++ src/javavideo/ifelserunner.java
@@ -0,0 +1,25 @@
+package javavideo;
+import java.util.*;
+public class ifelserunner {
+
+	public static void main(String[] args) {
+		
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number1: ");
+		int number1 = scanner.nextInt();
+		System.out.println("Enter number2: ");
+		int number2 = scanner.nextInt();
+		
+		ifelse.printChoice();
+		
+		System.out.println("Enter choice: ");
+		int choice = scanner.nextInt();
+		System.out.println("your choices are: "+choice);
+		
+		scanner.close();
+	}
+	
+
+		
+}
