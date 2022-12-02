4ddb32a78316ab87ac5d4045c3477fe13a11082c
mimi5963 <mimi5963@naver.com>
2022-12-02 15:14:48 +0900

    ㅗㅗ

--- src/javavideo/ifelserunner.java
+++ src/javavideo/ifelserunner.java
@@ -5,19 +5,31 @@ public class ifelserunner {
 	public static void main(String[] args) {
 		
 	
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter number1: ");
-		int number1 = scanner.nextInt();
-		System.out.println("Enter number2: ");
-		int number2 = scanner.nextInt();
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Enter number1: ");
+//		int number1 = scanner.nextInt();
+//		System.out.println("Enter number2: ");
+//		int number2 = scanner.nextInt();
+//		
+//		ifelse.printChoice();
+//		
+//		System.out.println("Enter choice: ");
+//		int choice = scanner.nextInt();
+//		System.out.println("your choices are: "+choice);
+//		
+//		scanner.close();
 		
-		ifelse.printChoice();
+		String someString ="This is a lot of text again";
+		for(int i=0; i< someString.length();i++)
+			System.out.println(someString.charAt(i));
 		
-		System.out.println("Enter choice: ");
-		int choice = scanner.nextInt();
-		System.out.println("your choices are: "+choice);
+		for(char a: someString.toCharArray())
+			{if(a==' ')continue;
+			System.out.println(a);
+			}
 		
-		scanner.close();
+		StringBuilder sb =new StringBuilder("hi");
+		
 	}
 	
 
---
+++ src/javavideo/manyclass.java
@@ -0,0 +1,18 @@
+package javavideo;
+
+public class manyclass {
+	public static void main(String[] args) {
+		
+		int i = 5;
+		Integer integer = new Integer(5); //생성자사용하거나
+		
+		Integer interger2= Integer.valueOf(5); //static method 사용하기
+		
+		Integer interger3 = Integer.valueOf("1234");
+		
+		System.out.println(interger2);
+		
+		
+		
+	}
+}
