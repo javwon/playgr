cd56016b6a0233f00601fbdaa9cb26fdc7fc5d5c
mimi5963 <mimi5963@naver.com>
2022-12-28 20:06:44 +0900

    exc

---
+++ src/javajungsuk/ex/Exercise8_1.java
@@ -0,0 +1,35 @@
+package javajungsuk.ex;
+import java.io.*;
+public class Exercise8_1 {
+
+	public static void main(String[] args) {
+	
+		try {
+			File f = createFile(new String("파일명"));
+			System.out.println(f.getName()+"파일이 정상적으로 생성됨");
+		}catch(Exception e) {
+			System.out.println("다시 입력해주세요");
+		}
+			
+	
+			
+	}
+	static File createFile(String filename) throws Exception{
+		if(filename == null || filename.equals("")) {
+			throw new Exception("문제발생");
+		}
+		File f = new File(filename);
+		createNewFile(f);
+		return f;
+	}
+	static void createNewFile(File f) throws Exception {
+		f.createNewFile();
+		
+	}
+
+}
+
+
+
+
+
