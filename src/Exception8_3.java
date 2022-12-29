39cb53b15157dd620a5bd4861fdaf8d6def661b4
mimi5963 <mimi5963@naver.com>
2022-12-29 19:49:09 +0900

    utf

--- src/Exception8_3.java
+++ src/Exception8_3.java
@@ -1,20 +1,29 @@
-
+import java.util.StringJoiner;
 public class Exception8_3 {
 
-	public static void main(String[] args) {
-		Excep ex = new Excep("hi");
-		Excep1 ex1 = new Excep1("me");
+	public static void main(String[] args) throws Exception{
+	String str = "가";
+	
+	byte[] arrb = str.getBytes("UTF-8");
+	byte[] arrd = str.getBytes("CP949");
+	
+	System.out.println("utf-8 :" + joinBytes(arrb));
+	System.out.println("utf-8 :" + joinBytes(arrd));
+	
+	String arrb1 = new String(arrb,"UTF-8");
+	String arrd1 = new String(arrd,"CP949");
+	
+	System.out.println(arrb1+arrd1);
+	}
+	
+	static String joinBytes(byte[] arr) {
+		StringJoiner ie = new StringJoiner(":","[","]");
 		
-		ex1.initCause(ex);
-		
+		for(byte a : arr) {
+			ie.add(String.format("%02x", a));
+		}
+		return ie.toString();
 	}
 
 }
 
-class Excep extends RuntimeException{
-	Excep(String ar){super(ar);}
-}
-
-class Excep1 extends Exception{
-	Excep1(String ar){super(ar);}
-}
