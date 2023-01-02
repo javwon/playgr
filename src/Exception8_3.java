5016bc0a79aacc56132ac5fb54a783f7c07db808
장원 <@DESKTOP-MRT7GU2>
2023-01-02 12:10:49 +0900

    브1

--- src/Exception8_3.java
+++ src/Exception8_3.java
@@ -1,41 +1,53 @@
-import static java.math.BigDecimal.*;
-import java.math.MathContext;
-import static java.math.RoundingMode.*;
-import java.util.*;
-import java.math.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+
 public class Exception8_3 {
 
 	
-
-	public static void main(String[] args) throws Exception{
-		BigDecimal bd1 = new BigDecimal("123.456"); 
-		BigDecimal bd2 = new BigDecimal("1.0"); 
-
-		System.out.print("bd1="+bd1);
-		System.out.print(",\tvalue="+bd1.unscaledValue());
-		System.out.print(",\tscale="+bd1.scale());		 
-		System.out.print(",\tprecision="+bd1.precision());
-		System.out.println();
-
-		System.out.print("bd2="+bd2);
-		System.out.print(",\tvalue="+bd2.unscaledValue());
-		System.out.print(",\tscale="+bd2.scale());		 
-		System.out.print(",\tprecision="+bd2.precision());
-		System.out.println();
-
-		
-		BigDecimal bd3 = bd1.multiply(bd2);
-		System.out.print("bd3="+bd3);
-		System.out.print(",\tvalue="+bd3.unscaledValue());
-		System.out.print(",\tscale="+bd3.scale());		 
-		System.out.print(",\tprecision="+bd3.precision());
-		System.out.println();
-
-		System.out.println(bd1.divide(bd2, 1)); // 123.46
-		System.out.println(bd1.setScale(4));    // 123.46
-		System.out.println(bd1.divide(bd2, new MathContext(4, HALF_UP)));
-	
-}	
-	
-
-}
+		 public static void main(String[] args) throws IOException{
+		       	BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+		        String input = s.readLine();
+		        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		        bw.write(mostused(input));
+		        bw.flush();
+		        bw.close();
+		        
+		    }
+		   static char mostused(String str) {
+			   int[] arr = new int[26];
+			   int Max_value_index=0;
+			   
+			   //문자 더하기
+			   for(int i =0; i<str.length();i++)
+			   {	char ch = str.charAt(i);
+			   		if(ch>='a'&&ch<='z') arr[ch-'a']++;
+			   		if(ch>='A'&&ch<='Z') arr[ch-'A']++;
+				   //arr[str.toUpperCase().charAt(i)-'A']++; 얘가 범인!! 
+			   }
+			   //Maxindex찾기
+			   for(int i=1;i<arr.length;i++)
+			   {
+					 
+				  if(arr[Max_value_index]<arr[i])
+				  {
+					  Max_value_index =i;
+				  }
+				  
+			   }
+			   //?찾기
+			   for(int i=0;i<arr.length;i++) {
+				   if(i!=Max_value_index) {
+					  if(arr[i]==arr[Max_value_index])
+						  return '?';
+				   }
+			   }
+			  
+			   return (char)(Max_value_index+'A');
+			   
+			   
+		   }
+		}
