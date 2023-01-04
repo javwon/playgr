8af04580aa8f115e5e0b85bbe984841db28c00a1
mimi5963 <mimi5963@naver.com>
2023-01-04 11:18:37 +0900

    stack and queue

--- src/object2/interfaces/ArrayListLinkedListTest.java
+++ src/object2/interfaces/ArrayListLinkedListTest.java
@@ -3,60 +3,35 @@ package object2.interfaces;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class ArrayListLinkedListTest {
 
 	public static void main(String[] args) {
-		ArrayList al = new ArrayList(2000000);
-		LinkedList ll = new LinkedList();
 		
-		System.out.println("==순차적 접근==");
-		System.out.println("ArrayList "+add1(al));
-		System.out.println("LinkedList"+add1(ll));
-		System.out.println();
-		System.out.println("==중간에 추가하기==");
-		System.out.println("ArrayList "+add2(al));
-		System.out.println("LinkedList "+add2(ll));
-		System.out.println();
-		System.out.println("=중간에서 삭제하기=");
-		System.out.println("ArrayList "+remove2(al));
-		System.out.println("LinkedList "+remove2(ll));
-		System.out.println();
-		System.out.println("= 순차적으로 삭제하기=");
-		System.out.println("ArrayList "+remove(al));
-		System.out.println("LinkedList "+remove(ll));
-		System.out.println();
+		Queue q = new LinkedList(); //shift + f2 Linkedlist가 queue구현
+		Stack s = new Stack();
+		
+		s.push("0");
+		s.push("1");
+		s.push("2");
+		
+		q.offer("0");
+		q.offer("1");
+		q.offer("2");
+		
+		System.out.println("=stack=");
+		while(!s.empty())
+			System.out.println(s.pop());
+		
+		System.out.println("=Queue");
+		while(!q.isEmpty())
+			System.out.println(q.poll());
+		
+	}
+
 	
-		
-		
-	}
-
-	private static long remove2(List list) {
-		long start = System.currentTimeMillis();
-		for(int i=0; i<10000;i++)list.remove(i);
-		long end = System.currentTimeMillis();
-		return end - start;
-	}
-
-	private static long add1(List list) {
-		long start = System.currentTimeMillis();
-		for(int i=0; i<1000000;i++)list.add(i+"");
-		long end = System.currentTimeMillis();
-		return end-start;
-	}
-
-	private static long add2(List list) {
-		long start = System.currentTimeMillis();
-		for(int i=0; i<10000;i++)list.add(500,"X");
-		long end = System.currentTimeMillis();
-		return end - start;
-	}
-
-	private static long remove(List list) {
-		long start = System.currentTimeMillis();
-		for(int i=list.size()-1;i>=0;i--)list.remove(i);
-		long end =System.currentTimeMillis();
-		return end-start;
-	}
+	
 
 }
