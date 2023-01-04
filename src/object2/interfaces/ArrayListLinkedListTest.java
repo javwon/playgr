fca8cd2713312b59739beb471c4b6cf821de95a1
mimi5963 <mimi5963@naver.com>
2023-01-04 11:42:12 +0900

    괄호 검사

--- src/object2/interfaces/ArrayListLinkedListTest.java
+++ src/object2/interfaces/ArrayListLinkedListTest.java
@@ -1,57 +1,44 @@
 package object2.interfaces;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
 public class ArrayListLinkedListTest {
-	public static Stack back = new Stack();
-	public static Stack forward = new Stack();
+	
 	public static void main(String[] args) {
-	//재밌다 혼자 해보기 
-	goURL("1.네이트");
-	goURL("2.야후");
-	goURL("3.네이버");
-	goURL("4.다음");
+	Stack st = new Stack();
+	String expression = "(2+3)*1";
 	
-	printStatus();
+	System.out.println("expression:" +expression);
 	
-	goBack(); // back에 있는 마지막 꺼내서 forward로 옮김 (후입선출)
-	System.out.println("뒤로가기 버튼 누름");
-	printStatus();
+	try {
+		for(int i=0; i<expression.length();i++) {
+			char ch = expression.charAt(i);
+			if(ch=='(') {
+				st.push(ch+"");
+			}else if(ch==')') {
+				st.pop();
+			}
+		}
+			if(st.isEmpty())System.out.println("괄호일치");
+			else System.out.println("괄호 불일치");
 		
-	goBack();
-	System.out.println("뒤로가기 버튼 누름");
-	printStatus();
+	}catch(EmptyStackException e) {System.out.println("괄호 일치 안합니다");}
 	
-	goForward(); // forward에 데이터 꺼내서 back으로 옮김
-	System.out.println("= 앞으로 가기 버튼 누른후=");
-	printStatus();
 	
-	goForward();
-	System.out.println("= 앞으로 가기 버튼 누른후=");
-	printStatus();
+	
+	
+	
 	
 	}
-	private static void goForward() {
-		back.add(forward.pop());
-		
-	}
-	private static void goBack() {
-		forward.add(back.pop());
+
 		
 		
-	}
-	private static void printStatus() {
-		System.out.println(back.peek());
-		
-	}
-	private static void goURL(Object s) {
-		back.add(s);
-		
-	}
+	
 
 	
 	
